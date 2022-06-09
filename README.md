99a19088efb82337e7b701bf82bea9e709a248ae
roguish <rgriffiths.1@gmail.com>
2022-06-09 21:20:26 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-qpnboj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-qpnboj)
---
+++ index.js
@@ -0,0 +1,3 @@
+tiddlywiki --versions// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
